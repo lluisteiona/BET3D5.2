9b17a5230899efbdca93120a3fc9be92fec71134
lluisteiona <lluisteiona@gmail.com>
2026-05-06 11:46:15 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/BET3D5.2/',   // ← AFEGEIX AQUESTA LÍNIA
   plugins: [react()],
-  base: '/BET3D5.2/',
 })
